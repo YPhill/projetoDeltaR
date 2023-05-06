369d9d91a1b0acb6d7603ba001b7789efbcf16c2
yphill <phelipequeiroz2004@gmail.com>
2023-05-05 21:48:45 -0300

    first commit

---
+++ Mobilitec-Services/frmCadastraPessoa.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mobilitec_Services
+{
+    public partial class frmCadastraPessoa : Form
+    {
+        public frmCadastraPessoa()
+        {
+            InitializeComponent();
+            carregaCabecalho();
+        }
+
+        DataTable dt = new DataTable();
+
+        public void carregaCabecalho()
+        {
+            dt.Columns.Add("Código", typeof(int));
+            dt.Columns.Add("Nome", typeof(string));
+            dt.Columns.Add("E-mail", typeof(string));
+            dt.Columns.Add("CPF", typeof(string));
+            dt.Columns.Add("Telefone", typeof(string));
+
+            dgvDadosPessoais.DataSource = dt;
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            dt.Rows.Add(txtCodigo.Text, txtNome.Text, txtEmail.Text, mskCPF.Text, mskTelefone.Text);
+            dgvDadosPessoais.DataSource = dt;
+            limparCampos();
+
+        }
+
+        public void limparCampos()
+        {
+            txtCodigo.Clear();
+            txtNome.Clear();
+            txtEmail.Clear();
+            mskCPF.Text = "";
+            mskTelefone.Text = "";
+            txtCodigo.Focus();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (dgvDadosPessoais.CurrentRow != null)
+            {
+                int linha = dgvDadosPessoais.CurrentCell.RowIndex;
+                dgvDadosPessoais.Rows.RemoveAt(linha);
+            }
+            else
+            {
+                MessageBox.Show("Dados excluídos");
+            }
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            dt.Rows.Clear();
+            dgvDadosPessoais.DataSource = dt;
+        }
+
+        private void dgvDadosPessoais_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txtCodigo.Text = dgvDadosPessoais.CurrentRow.Cells[0].Value.ToString();
+                txtNome.Text = dgvDadosPessoais.CurrentRow.Cells[1].Value.ToString();
+                txtEmail.Text = dgvDadosPessoais.CurrentRow.Cells[2].Value.ToString();
+                mskCPF.Text = dgvDadosPessoais.CurrentRow.Cells[3].Value.ToString();
+                mskTelefone.Text = dgvDadosPessoais.CurrentRow.Cells[4].Value.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dados inválidos!!!");
+            }
+
+        }
+    }
+}
---
+++ Mobilitec-Services/frmCarregaDataGridDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Mobilitec_Services
+{
+    public partial class frmCarregaDataGridDB : Form
+    {
+        public frmCarregaDataGridDB()
+        {
+            InitializeComponent();
+        }
+
+        DataTable dt = new DataTable();
+
+        private void btnCarregaDados_Click(object sender, EventArgs e)
+        {
+
+            dt.Rows.Clear();
+
+            dgvFuncionarios.DataSource = null;
+
+            //string sql1 = "select * from tbFuncionarios;";
+
+
+            string sql = "select codfunc as 'Código',nome as 'Nome',email as 'E-mail', cpf as 'CPF',telefone as 'Telefone',sexo as 'Sexo',   endereco as 'Endereço',numero as 'Número',cep as 'CEP', bairro as 'Bairro',cidade as 'Cidade',estado as 'Estado' from tbfuncionarios; ";
+
+            MySqlDataAdapter da = new MySqlDataAdapter(sql, Conexao.obterConexao());
+
+            da.Fill(dt);
+
+
+            dgvFuncionarios.DataSource = dt;
+
+            Conexao.fecharConexao();
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            dt.Rows.Clear();
+
+
+        }
+    }
+}
